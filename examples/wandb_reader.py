0b257793f000e67fb73dfa5ecb631eac2562e340
RandallBalestriero <randallbalestriero@gmail.com>
2024-09-03 10:26:48 -0400

    added project reader example

--- examples/wandb_reader.py
+++ examples/wandb_reader.py
@@ -1,4 +1,6 @@
 import stable_ssl as ssl
 
-data = ssl.utils.reader.wandb_run("excap", "single_dataset_sequential", "p67ng6bq")
-print(data)
+config, df = ssl.utils.reader.wandb_run("excap", "single_dataset_sequential", "p67ng6bq")
+print(df)
+configs, dfs = ssl.utils.reader.wandb_project("excap", "single_dataset_sequential")
+print(dfs)
