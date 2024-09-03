8cf0cee974e510cd23c2cb721aa7922765ca015f
RandallBalestriero <randallbalestriero@gmail.com>
2024-09-03 10:17:11 -0400

    update example w/ wandb

--- examples/wandb_reader.py
+++ examples/wandb_reader.py
@@ -1,4 +1,4 @@
 import stable_ssl as ssl
 
-data = ssl.utils.reader.read_wandb_run("excap", "single_dataset_sequential", "p67ng6bq")
+data = ssl.utils.reader.wandb_run("excap", "single_dataset_sequential", "p67ng6bq")
 print(data)
