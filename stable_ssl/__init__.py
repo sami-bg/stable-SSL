a27550661047aad409e913dcb424ec043a10b184
huguesva <vanasselhugues@gmail.com>
2024-09-04 08:34:01 +0200

    import submodules

--- stable_ssl/__init__.py
+++ stable_ssl/__init__.py
@@ -1,4 +1,20 @@
+from . import ssl_modules
+from . import config
+from . import utils
+from . import sampler
+from . import trainer
+from . import supervised
+
+
 from .ssl_modules import SimCLR
 from .config import get_args
 
-__all__ = ["SimCLR", "get_args"]
+__all__ = [
+    "ssl_modules",
+    "config",
+    "utils",
+    "sampler",
+    "trainer",
+    "supervised,
+    "SimCLR", 
+    "get_args"]
