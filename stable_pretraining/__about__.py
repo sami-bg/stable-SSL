7a0207ce90c336f5458aec229b38b56fd592bb37
Lucas Maes <43337476+lucas-maes@users.noreply.github.com>
2025-09-20 13:33:44 -0400

    add pypi publish (#297)

--- stable_pretraining/__about__.py
+++ stable_pretraining/__about__.py
@@ -7,9 +7,9 @@ __all__ = [
     "__url__",
 ]
 
-__title__ = "stable-ssl"
+__title__ = "stable-pretraining"
 __summary__ = "Self-Supervised Learning Research Library"
-__version__ = "0.0.1.dev0"
+__version__ = "v0.0.1"
 __author__ = "Randall Balestriero, Hugues Van Assel, Lucas Maes"
 __license__ = "MIT"
-__url__ = "https://rbalestr-lab.github.io/stable-SSL.github.io/"
+__url__ = "https://rbalestr-lab.github.io/stable-pretraining.github.io"
