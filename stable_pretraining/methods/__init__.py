e5c81f6cc800ad7cd726894f4d595f7956d06817
Sami <48739272+sami-bg@users.noreply.github.com>
2026-03-05 10:27:21 -0500

    [Feature] LeJEPA INet10 Single GPU benchmark (#394)

--- stable_pretraining/methods/__init__.py
+++ stable_pretraining/methods/__init__.py
@@ -1,5 +1,6 @@
 from .ijepa import IJEPA
 from .mae import MAE
+from .lejepa import LeJEPA
 from .nepa import NEPA
 
-__all__ = ["IJEPA", "MAE", "NEPA"]
+__all__ = ["IJEPA", "MAE", "LeJEPA", "NEPA"]
